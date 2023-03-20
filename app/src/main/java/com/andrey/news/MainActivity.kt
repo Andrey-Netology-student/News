9a439aacccde2321b3c2c0cacab61175d59522dc
Andrey B <andreychitry@mail.ru>
2023-03-20 22:58:19 +0300

    "Errors xml"

--- app/src/main/java/com/andrey/news/MainActivity.kt
+++ app/src/main/java/com/andrey/news/MainActivity.kt
@@ -2,10 +2,28 @@ package com.andrey.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.andrey.news.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val mBinding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater) //Инициализируем _binding. layoutInflater пишется с маленькой буквы!
+        setContentView(R.layout.fragment_splash)
+        Handler(Looper.myLooper()!!).postDelayed({
+            setContentView(mBinding.root)
+            bottom_nav_menu.setupWithNavController(
+                navController = nav_host_fragment.findNavController()
+            )
+        }, 5000)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }

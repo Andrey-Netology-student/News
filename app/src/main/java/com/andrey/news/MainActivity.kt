fe5436171472becd6620d61536563f0c3cf8697f
Andrey B <andreychitry@mail.ru>
2023-03-23 18:33:08 +0300

    "Error"

--- app/src/main/java/com/andrey/news/MainActivity.kt
+++ app/src/main/java/com/andrey/news/MainActivity.kt
@@ -4,20 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.navigation.ui.setupWithNavController
 import com.andrey.news.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
-    private val mBinding get() = _binding!!
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater) //Инициализируем _binding. layoutInflater пишется с маленькой буквы!
         setContentView(R.layout.fragment_splash)
         Handler(Looper.myLooper()!!).postDelayed({
-            setContentView(mBinding.root)
-            bottom_nav_menu.setupWithNavController(
-                navController = nav_host_fragment.findNavController()
+            setContentView(binding.root)
+            binding.bottomNavMenu.setupWithNavController(
+                navController = binding.nav_host_fragment.findNavController()
             )
         }, 5000)
     }
